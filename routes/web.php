167cf9f841c0bc0a2db85f2ac367e3775102c723
tomita <sugawara.an@gmail.com>
2018-01-13 21:04:24 +0900

    ログイン画面作成

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'RegistrationController@index');
 
 Route::get('twitter/login', 'TwitterController@login')->name('twitter.login');
 Route::get('twitter/callback', 'TwitterController@callback')->name('twitter.callback');
